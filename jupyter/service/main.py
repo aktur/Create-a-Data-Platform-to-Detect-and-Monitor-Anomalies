2f77fc80404aaebb4da128a7509cf28414fe3ad4
Michal Rutka <michal.rutka@gmail.com>
2021-03-21 19:24:33 +0100

    Milestone 6

--- jupyter/service/main.py
+++ jupyter/service/main.py
@@ -3,28 +3,45 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from joblib import load
 
+import prometheus_client
+from prometheus_client import Counter, Histogram
+import time
 
 app = FastAPI()
 model = load("model.joblib")
+
+metrics_app = prometheus_client.make_asgi_app()
+app.mount("/metrics", metrics_app)
 
 class Item(BaseModel):
     feature_vector: List[float]
     score: Optional[bool] = None
 
+mi = Counter('model_information', 'Model counter')
+pr = Counter('prediction', 'Prediction counter')
+hi = Histogram('prediction_response', 'Prediction histogram')
+sc = Histogram('score_response', 'Score histogram')
+lt = Histogram('response_latency', 'Latency histogram')
 
 @app.get("/model_information")
 def read_root():
+    mi.inc()
     return model.get_params()
 
 
 @app.post("/prediction")
 def predict(request: Item):
+    pr.inc()
     response = {}
 
     # Call model.predict(...) and
     # add to response the returned prediction,
     # For example, response["is_inlier"] = int(prediction[0])
+    start = time.time()
     prediction = model.predict([request.feature_vector])
+    end = time.time()
+    lt.observe(end-start)
+    hi.observe(prediction)
     response = {"is_inlier": int(prediction[0])}
 
     # Check if request.score is True, also call model.score_samples
@@ -32,6 +49,7 @@ def predict(request: Item):
     # response the returned anomaly score.
     if request.score is True:
         score = model.score_samples([request.feature_vector])
+        sc.observe(score)
         response["anomaly_score"] = score[0]
 
     return response
